7328ea170b1e1eb76083604421538ad0cd1e1322
Hrenaki <www.shikhovwtr@gmail.com>
2023-03-12 13:39:24 +0300

    Added bitmap matrix load optimizations

---
+++ optimizations/JPEG.Benchmarks/Benchmarks/BitmapLoadBenchmark.cs
@@ -0,0 +1,53 @@
+﻿using BenchmarkDotNet.Attributes;
+using JPEG.Images;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPEG.Benchmarks.Benchmarks
+{
+   [MemoryDiagnoser]
+   [SimpleJob(warmupCount: 2, iterationCount: 3)]
+   public class BitmapLoadBenchmark
+   {
+      private static readonly string imagePath = @"sample.bmp";
+
+      [Benchmark]
+      public void ReadBitmapWithCast()
+      {
+         using var fileStream = File.OpenRead(imagePath);
+         using var bmp = (Bitmap)Image.FromStream(fileStream, false, false);
+         var imageMatrix = (Matrix)bmp;
+
+         FakeMatrixSave(imageMatrix);
+      }
+
+      [Benchmark]
+      public void ReadRawBytes()
+      {
+         using var fileStream = File.OpenRead(imagePath);
+         using var bmp = (Bitmap)Image.FromStream(fileStream, false, false);
+         var imageMatrix = Matrix.FromBitmap(bmp);
+
+         FakeMatrixSave(imageMatrix);
+      }
+
+      private static void FakeMatrixSave(Matrix matrix)
+      {
+         File.WriteAllText("123.txt", FakeMatrixToString(matrix));
+      }
+
+      private static string FakeMatrixToString(Matrix matrix)
+      {
+         var sb = new StringBuilder();
+         for (var j = 0; j < 3; j++)
+         {
+            sb.Append("byte;");
+         }
+         return sb.ToString();
+      }
+   }
+}
--- optimizations/JPEG/Images/Matrix.cs
+++ optimizations/JPEG/Images/Matrix.cs
@@ -1,65 +1,105 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace JPEG.Images;
 
-class Matrix
+public class Matrix
 {
-	public readonly Pixel[,] Pixels;
-	public readonly int Height;
-	public readonly int Width;
+   public Pixel[,] Pixels { get; private init; }
+   public int Height { get; private init; }
+   public int Width { get; private init; }
 
-	public Matrix(int height, int width)
-	{
-		Height = height;
-		Width = width;
+   public Matrix(int height, int width)
+   {
+      Height = height;
+      Width = width;
 
-		Pixels = new Pixel[height, width];
-		for (var i = 0; i < height; ++i)
-		for (var j = 0; j < width; ++j)
-			Pixels[i, j] = new Pixel(0, 0, 0, PixelFormat.RGB);
-	}
+      Pixels = new Pixel[height, width];
+      for (var i = 0; i < height; ++i)
+         for (var j = 0; j < width; ++j)
+            Pixels[i, j] = new Pixel(0, 0, 0, PixelFormat.RGB);
+   }
 
-	public static explicit operator Matrix(Bitmap bmp)
-	{
-		var height = bmp.Height - bmp.Height % 8;
-		var width = bmp.Width - bmp.Width % 8;
-		var matrix = new Matrix(height, width);
+   private Matrix(Pixel[,] pixels)
+   {
+      ArgumentNullException.ThrowIfNull(pixels);
 
-		for (var j = 0; j < height; j++)
-		{
-			for (var i = 0; i < width; i++)
-			{
-				var pixel = bmp.GetPixel(i, j);
-				matrix.Pixels[j, i] = new Pixel(pixel.R, pixel.G, pixel.B, PixelFormat.RGB);
-			}
-		}
+      Height = pixels.GetLength(0);
+      Width = pixels.GetLength(1);
+      Pixels = pixels;
+   }
 
-		return matrix;
-	}
+   public unsafe static Matrix FromBitmap(Bitmap bmp)
+   {
+      var height = bmp.Height - bmp.Height % 8;
+      var width = bmp.Width - bmp.Width % 8;
 
-	public static explicit operator Bitmap(Matrix matrix)
-	{
-		var bmp = new Bitmap(matrix.Width, matrix.Height);
+      var data = bmp.LockBits(new Rectangle(new Point(), new Size(bmp.Width, bmp.Height)),
+                              System.Drawing.Imaging.ImageLockMode.ReadOnly,
+                              System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+      var ptr = data.Scan0;
 
-		for (var j = 0; j < bmp.Height; j++)
-		{
-			for (var i = 0; i < bmp.Width; i++)
-			{
-				var pixel = matrix.Pixels[j, i];
-				bmp.SetPixel(i, j, Color.FromArgb(ToByte(pixel.R), ToByte(pixel.G), ToByte(pixel.B)));
-			}
-		}
+      var rgbBuffer = new byte[3];
 
-		return bmp;
-	}
+      var pixelMatrix = new Pixel[height, width];
+      for (var j = 0; j < height; j++)
+      {
+         for (var i = 0; i < width; i++)
+         {
+            Marshal.Copy(ptr, rgbBuffer, 0, 3);
+            pixelMatrix[j, i] = new Pixel(rgbBuffer[0], rgbBuffer[1], rgbBuffer[2], PixelFormat.RGB);
 
-	public static int ToByte(double d)
-	{
-		var val = (int)d;
-		if (val > byte.MaxValue)
-			return byte.MaxValue;
-		if (val < byte.MinValue)
-			return byte.MinValue;
-		return val;
-	}
+            ptr += 3;
+         }
+
+         ptr += bmp.Width % 8 * 3;
+      }
+
+      return new Matrix(pixelMatrix);
+   }
+
+   public static explicit operator Matrix(Bitmap bmp)
+   {
+      var height = bmp.Height - bmp.Height % 8;
+      var width = bmp.Width - bmp.Width % 8;
+      var matrix = new Matrix(height, width);
+
+      for (var j = 0; j < height; j++)
+      {
+         for (var i = 0; i < width; i++)
+         {
+            var pixel = bmp.GetPixel(i, j);
+            matrix.Pixels[j, i] = new Pixel(pixel.R, pixel.G, pixel.B, PixelFormat.RGB);
+         }
+      }
+
+      return matrix;
+   }
+
+   public static explicit operator Bitmap(Matrix matrix)
+   {
+      var bmp = new Bitmap(matrix.Width, matrix.Height);
+
+      for (var j = 0; j < bmp.Height; j++)
+      {
+         for (var i = 0; i < bmp.Width; i++)
+         {
+            var pixel = matrix.Pixels[j, i];
+            bmp.SetPixel(i, j, Color.FromArgb(ToByte(pixel.R), ToByte(pixel.G), ToByte(pixel.B)));
+         }
+      }
+
+      return bmp;
+   }
+
+   public static int ToByte(double d)
+   {
+      var val = (int)d;
+      if (val > byte.MaxValue)
+         return byte.MaxValue;
+      if (val < byte.MinValue)
+         return byte.MinValue;
+      return val;
+   }
 }
